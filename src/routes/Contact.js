36b40353411a29cf771fb81809cee37b13f4e8fe
natnael <natnaelger@gmail.com>
2024-08-31 07:58:38 -0500

    wip

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,69 +1,69 @@
 import * as React from "react";
-// import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import * as yup from "yup";
-// import axios from "axios";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
 import {
   Box,
   Button,
   CardMedia,
   Container,
-  // FormControl,
+  FormControl,
   Grid,
-  // TextField,
+  TextField,
   Typography,
 } from "@mui/material";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
-// import CircularProgress from "@mui/material/CircularProgress";
+import CircularProgress from "@mui/material/CircularProgress";
 
-// import apiConfig from "../config/apiConfig";
+import apiConfig from "../config/apiConfig";
 import { DISPLAY_PHONE_NUMBER, EMAIL } from "../constants/constants";
 
-// const schema = yup
-//   .object({
-//     name: yup.string().required("Name is required"),
-//     email: yup
-//       .string()
-//       .required("Email is required")
-//       .email("Email must be in this format; email@email.com"),
-//     message: yup.string().required("Message is required"),
-//   })
-//   .required();
+const schema = yup
+  .object({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .required("Email is required")
+      .email("Email must be in this format; email@email.com"),
+    message: yup.string().required("Message is required"),
+  })
+  .required();
 
 export const Contact = () => {
-  // const [success, setSuccess] = React.useState(false);
-  // const [isLoading, setIsLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm({ resolver: yupResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
-  // const onSubmit = async ({ name, email, message }) => {
-  //   const formData = new FormData();
-  //   formData.append("name", name);
-  //   formData.append("email", email);
-  //   formData.append("message", message);
+  const onSubmit = async ({ name, email, message }) => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
 
-  //   try {
-  //     setIsLoading(true);
+    try {
+      setIsLoading(true);
 
-  //     const url = `${apiConfig.api.baseUrl}/v1/quote`;
-  //     const response = await axios.post(url, formData);
+      const url = `${apiConfig.api.baseUrl}/v1/quote`;
+      const response = await axios.post(url, formData);
 
-  //     setSuccess(response.status === 200);
-  //   } catch (err) {
-  //     setSuccess(false);
-  //   }
+      setSuccess(response.status === 200);
+    } catch (err) {
+      setSuccess(false);
+    }
 
-  //   setIsLoading(false);
-  // };
+    setIsLoading(false);
+  };
 
   return (
     <Box sx={{ bgcolor: "#fff" }}>
-      <Container maxWidth="lg" sx={{ pt: 16, pb: 4 }}>
+      <Container maxWidth="lg" sx={{ pt: 8, pb: 4 }}>
         <Grid container spacing={4}>
           <Grid
             item
@@ -149,11 +149,11 @@ export const Contact = () => {
                 </Button>
               </Box>
 
-              {/* <FormControl
+              <FormControl
                 sx={{
                   display: "flex",
                   flexDirection: "column",
-                  mt: 4,
+                  mt: 3,
                 }}
               >
                 <TextField
@@ -209,7 +209,7 @@ export const Contact = () => {
                     )}
                   </Button>
                 </Box>
-              </FormControl> */}
+              </FormControl>
             </Box>
           </Grid>
 
@@ -221,7 +221,7 @@ export const Contact = () => {
           >
             <CardMedia
               component="img"
-              image={"/img/test.png"}
+              image={"/img/front-page.jpg"}
               sx={{ width: "100%", height: "100%", objectFit: "contain" }}
             />
           </Grid>
